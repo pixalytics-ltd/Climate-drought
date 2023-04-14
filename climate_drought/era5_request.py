b127219d3d121e08b57510add962303ca6358821
sam-pixalytics <pixalytics@users.noreply.github.com>
2023-04-14 08:06:37 +0100

    Tidying code #11

--- climate_drought/era5_request.py
+++ climate_drought/era5_request.py
@@ -25,10 +25,12 @@ logging.basicConfig(level=logging.INFO)
 
 # Shared constants
 PRECIP_VARIABLES = ['total_precipitation']
-SOILWATER_VARIABLES = ["volumetric_soil_water_layer_1","volumetric_soil_water_layer_2","volumetric_soil_water_layer_3","volumetric_soil_water_layer_4"]
+SOILWATER_VARIABLES = ["volumetric_soil_water_layer_1", "volumetric_soil_water_layer_2",
+                       "volumetric_soil_water_layer_3", "volumetric_soil_water_layer_4"]
 
 AWSKEY = os.path.join(expanduser('~'), '.aws_api_key')
 AWS_PRECIP_VARIABLE = ['precipitation_amount_1hour_Accumulation']
+
 
 class ERA5Request():
     """
@@ -38,7 +40,9 @@ class ERA5Request():
     - baseline = True: a monthly mean over a long time period to form the mean or baseline against which anomalies can be computed
     - baseline = False: a monthly or hourly value to retrieve data over shorter timescales
     """
-    def __init__(self, variables, fname_out, args: config.AnalysisArgs, config: config.Config, baseline=False, aws=False, monthly=True):
+
+    def __init__(self, variables, fname_out, args: config.AnalysisArgs, config: config.Config, baseline=False,
+                 aws=False, monthly=True):
         self.latitude = args.latitude
         self.longitude = args.longitude
         self.start_date = config.baseline_start if baseline else args.start_date
@@ -50,6 +54,7 @@ class ERA5Request():
         self.aws = aws
         self.monthly = baseline or monthly
 
+
 class ERA5Download():
     """
     Provides some basic functionality that can be used by different implementation specific strategies for different
@@ -59,7 +64,7 @@ class ERA5Download():
     #   target download time for each data source
     SAMPLE_TIME = time(hour=12, minute=0)
 
-    def __init__(self, req : ERA5Request, logger: logging.Logger):
+    def __init__(self, req: ERA5Request, logger: logging.Logger):
         """
         Initializer; should be called by derived classes
         :param args: program arguments
@@ -71,13 +76,13 @@ class ERA5Download():
         self.req = req
 
         # Create list of dates between max start and end dates
-        dates = utils.daterange(self.req.start_date,self.req.end_date,0)
+        dates = utils.daterange(self.req.start_date, self.req.end_date, 0)
         date_list = []
         for indate in dates:
             yyyy = int(indate[0:4])
             mm = int(indate[4:6])
             dd = int(indate[6:8])
-            date_list.append(date(yyyy,mm,dd))
+            date_list.append(date(yyyy, mm, dd))
         self.dates = date_list
 
     @property
@@ -87,14 +92,16 @@ class ERA5Download():
         :return: path to the file that will be downloaded
         """
         freq = 'monthly' if self.req.monthly else 'hourly'
-        file_str = "{sd}-{ed}_{fq}_{la}_{lo}".format(sd=self.req.start_date, ed=self.req.end_date, fq=freq, la=self.req.latitude, lo=self.req.longitude)
+        file_str = "{sd}-{ed}_{fq}_{la}_{lo}".format(sd=self.req.start_date,
+                                                     ed=self.req.end_date, fq=freq, la=self.req.latitude,
+                                                     lo=self.req.longitude)
         # Extra identifier for AWS downloaded ERA5 data
         if not self.req.aws:
             aws = ''
         else:
             aws = '-aws'
-        return os.path.join(self.req.working_dir, self.req.fname_out + "_{d}{a}.nc".format(d=file_str,a=aws))
-    
+        return os.path.join(self.req.working_dir, self.req.fname_out + "_{d}{a}.nc".format(d=file_str, a=aws))
+
     def download(self) -> str:
         """
         This function handles downloading from the Copernicus Climate Service.
@@ -107,11 +114,10 @@ class ERA5Download():
 
         # Setup area of interest extraction
         boxsz = 0.1
-        area_box = []
-        area_box.append(round(float(self.req.latitude) + boxsz,2))
-        area_box.append(round(float(self.req.longitude) - boxsz,2))
-        area_box.append(round(float(self.req.latitude) - boxsz,2))
-        area_box.append(round(float(self.req.longitude) + boxsz,2))
+        area_box = [round(float(self.req.latitude) + boxsz, 2),
+                    round(float(self.req.longitude) - boxsz, 2),
+                    round(float(self.req.latitude) - boxsz, 2),
+                    round(float(self.req.longitude) + boxsz, 2)]
 
         if not self.req.monthly:
             times = []
@@ -138,7 +144,8 @@ class ERA5Download():
 
         return self.download_file_path
 
-    def _download_era5_data(self, variables: List[str], dates: List[date], times: List[time], area: str, monthly: str, out_file: str) -> bool:
+    def _download_era5_data(self, variables: List[str], dates: List[date], times: List[time], area: List[float],
+                            monthly: str, out_file: str) -> bool:
 
         """
         Executes the ERA5 download script in a separate process.
@@ -158,24 +165,26 @@ class ERA5Download():
             else:
                 era_monthly = False
 
-            self.logger.info("Downloading ERA data for {} {} for {}".format(dates[0],dates[-1],area))
-            result = era_download.download_era5_reanalysis_data(dates=dates, times=times, variables=variables, area=str(area),
-                                          monthly=era_monthly, file_path=os.path.expanduser(out_file))
+            self.logger.info("Downloading ERA data for {} {} for {}".format(dates[0], dates[-1], area))
+            result = era_download.download_era5_reanalysis_data(dates=dates,
+                                                                times=times, variables=variables, area=str(area),
+                                                                monthly=era_monthly,
+                                                                file_path=os.path.expanduser(out_file))
 
             if result == 0:
                 raise RuntimeError("Download process returned unexpected non-zero exit code '{}'.".format(result))
-            
+
         else:
             self.logger.info("Download file '{}' already exists.".format(out_file))
             outfile_exists = True
 
         if not os.path.isfile(out_file):
             raise FileNotFoundError("Output file '{}' could not be located.".format(out_file))
-        
+
         return outfile_exists
 
     # Created with reference to https://medium.com/pangeo/fake-it-until-you-make-it-reading-goes-netcdf4-data-on-aws-s3-as-zarr-for-rapid-data-access-61e33f8fe685
-    def _download_aws_data(self, area: str, out_file: str) -> bool:
+    def _download_aws_data(self, area: List[float], out_file: str) -> bool:
 
         """
         Executes the AWS download script in a separate process.
@@ -187,23 +196,27 @@ class ERA5Download():
         outfile_exists = False
 
         if not os.path.exists(out_file):
-            self.logger.info("Downloading ERA data for {} {} for {}".format(self.req.start_date, self.req.end_date, area))
+            self.logger.info(
+                "Downloading ERA data for {} {} for {}".format(self.req.start_date, self.req.end_date, area))
 
             # Get list of AWS files
             fs = fsspec.filesystem('s3', anon=True)
-            years = list(np.arange(int(self.req.start_date[0:4]), int(self.req.end_date[0:4])+1, 1))
-            months = list(np.arange(1,12+1,1))
+            sdate = int(self.req.start_date[0:4])
+            edate = 2020  # int(self.req.end_date[0:4])+1
+            years = list(np.arange(sdate, 2020, 1))
+            self.logger.warning("AWS range restricted to {} to 2020 as the files after cause issues".format(sdate))
+            months = list(np.arange(1, 12 + 1, 1))
 
             urls = []
             for year in years:
                 for month in months:
                     if month < 10:
-                        mnth = '0'+str(month)
+                        mnth = '0' + str(month)
                     else:
                         mnth = month
 
                     # ERA5-pds is located in us-west-2 and so depending on where this computation is taking place the time taken can vary dramatically.
-                    s3file = "s3://era5-pds/{}/{}/data/{}.nc".format(year,mnth,AWS_PRECIP_VARIABLE[0])
+                    s3file = "s3://era5-pds/{}/{}/data/{}.nc".format(year, mnth, AWS_PRECIP_VARIABLE[0])
                     url = fs.glob(s3file)
                     # Check the file exists online
                     if len(url) > 0:
@@ -212,7 +225,7 @@ class ERA5Download():
             self.logger.info("Example S3 URL: {}".format(urls[0]))
 
             # Create path for generated JSON files
-            jdir = os.path.join(self.req.working_dir,'jsons')
+            jdir = os.path.join(self.req.working_dir, 'jsons')
             pathlib.Path(jdir).mkdir(exist_ok=True)
 
             # Extract JSON files
@@ -229,7 +242,7 @@ class ERA5Download():
                 dirlist = os.path.dirname(os.path.dirname(u))
                 mnth = os.path.basename(dirlist)
                 yr = os.path.basename(os.path.dirname(dirlist))
-                jfile = os.path.join(jdir,"{}-{}-aws-precip.json".format(yr,mnth))
+                jfile = os.path.join(jdir, "{}-{}-aws-precip.json".format(yr, mnth))
                 if not os.path.exists(jfile):
                     with fsspec.open(u, **so) as inf:
                         h5chunks = SingleHdf5ToZarr(inf, u, inline_threshold=300)
@@ -273,8 +286,9 @@ class ERA5Download():
                         remote_options={'anon': True},
                         concat_dims=['time1'],
                         identical_dims=['lat', 'lon'],
-                        #inline_threshold=0,
-                        postprocess=postprocess # 0 lon to Nan is as a result of no fill_value being assigned
+                        # inline_threshold=0,
+                        postprocess=postprocess
+                        # 0 lon to Nan is as a result of no fill_value being assigned, so postprocess
                     )
                     d = mzz.translate()
                     with fs2.open(jfile, 'wb') as f:
@@ -283,8 +297,8 @@ class ERA5Download():
                 year_jlist.append(jfile)
 
             # Make combined JSON file
-            ## Concatenate along a specified dimension using the concat_dims argument
-            ## Specifying the identical coordinate across the files using the identical_dims argument is not strictly necessary but will speed up computation times.
+            ## Concatenate along a specified dimension (concat_dims)
+            ## Specifying identical coordinates (identical_dims) is not strictly necessary but will speed up computation times.
             mzz = MultiZarrToZarr(
                 year_jlist,
                 remote_protocol="s3",
@@ -293,7 +307,7 @@ class ERA5Download():
                 identical_dims=['lat', 'lon'],
                 inline_threshold=0
             )
-            cfile = os.path.join(jdir,'combined.json')
+            cfile = os.path.join(jdir, 'combined.json')
             mzz.translate(cfile)
 
             # Import JSON into xarray
@@ -305,8 +319,6 @@ class ERA5Download():
                 skip_instance_cache=True
             )
             m = fs.get_mapper("")
-            backend_args = {"consolidated": False,
-                            "storage_options": {"fo": d, "remote_protocol": "s3", "remote_options": {"anon": True}}}
             ds = xr.open_dataset(m, engine='zarr')
 
             # Prepare to extract lat/lon subset
@@ -317,15 +329,16 @@ class ERA5Download():
             minlat = float(area[2])
             maxlat = float(area[0])
             ## Change Latitude to 0 to 360 from -180 to 180
-            minlon = float(area[1])+180.0
-            maxlon = float(area[3])+180.0
-            self.logger.info("Extraction range, Lat: {:.3f} {:.3f} Lon: {:.3f} {:.3f}".format(minlat,maxlat,minlon,maxlon))
+            minlon = float(area[1]) + 180.0
+            maxlon = float(area[3]) + 180.0
+            self.logger.info(
+                "Extraction range, Lat: {:.3f} {:.3f} Lon: {:.3f} {:.3f}".format(minlat, maxlat, minlon, maxlon))
             mask_lon = (ds.lon >= minlon) & (ds.lon <= maxlon)
             mask_lat = (ds.lat >= minlat) & (ds.lat <= maxlat)
             ds_subset = ds.where(mask_lon & mask_lat, drop=True)
 
             # Rename variable names
-            ds_subset = ds_subset.rename({'lon': 'longitude','lat': 'latitude',AWS_PRECIP_VARIABLE[0]:'tp'})
+            ds_subset = ds_subset.rename({'lon': 'longitude', 'lat': 'latitude', AWS_PRECIP_VARIABLE[0]: 'tp'})
             self.logger.info(ds_subset)
 
             # Write to NetCDF
