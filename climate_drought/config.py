1ef008d2cb66f56ce08132b14da7f35abdba3185
eejco <eejco@leeds.ac.uk>
2023-04-20 17:49:37 +0100

    add table column names to config

--- climate_drought/config.py
+++ climate_drought/config.py
@@ -6,9 +6,12 @@ class AnalysisArgs():
         self.end_date = end_date
 
 class CDIArgs(AnalysisArgs):
-    def __init__(self, latitude, longitude, start_date, end_date, sma_source='EDO'):
+    def __init__(self, latitude, longitude, start_date, end_date, sma_source='EDO', spi_var='spi', sma_var='smant', fpr_var='fpanv'):
         super().__init__(latitude, longitude, start_date, end_date)
         self.sma_source = sma_source
+        self.spi_var = spi_var
+        self.sma_var = sma_var
+        self.fpr_var = fpr_var
 
 class Config():
     def __init__(self,outdir='output',indir='input',verbose=True,baseline_start='19850101',baseline_end='20221231'):
