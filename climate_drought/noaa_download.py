90271d6a49380ee5d74a514c09e9812201bd6cbe
eejco <92084382+eejco@users.noreply.github.com>
2023-05-29 11:31:33 +0100

    Feature/19 (#25)
    * working
    
    * working changes
    
    * remove test scripts
    
    * add nclimgrid spi option from nclimgrid
    
    * Added missing date back into GeoJSON #19
    
    * Swap year and day #19
    
    ---------
    
    Co-authored-by: Jenny <jennifercocks94@gmail.com>
    Co-authored-by: sam-laptop-vm <seadas-user@localhost>

---
+++ climate_drought/noaa_download.py
@@ -0,0 +1,32 @@
+import ssl
+
+from urllib.request import urlretrieve
+from datetime import datetime
+from enum import Enum
+
+URL_POSITION = "https://edr-api-c.mdl.nws.noaa.gov/Climate-EDR//collections/{collection}/position?coords=POINT({lon}%20{lat})&parameter-name={param}&datetime={start}/{end}&crs=EPSG:4326&f=csv"
+
+class NClimGridParams(Enum):
+    PRECIPITATION = 'prcp'
+    TEMPERATURE_MAX = 'tmax'
+    TEMPERATURE_MIN = 'tmin'
+
+
+def get_nclimgrid(lon, lat, start: str, end: str, param: NClimGridParams, out_filepath: str):
+    """"
+    Returns a dataframe of the parameter 'param' from the nclimgrid_monthly collection
+    :param start: the start date in the format 'YYYYMMDD'
+    :param end: the end date in the format 'YYYYMMDD'
+    """
+    str2dt = lambda str: datetime(int(str[0:4]),int(str[4:6]),int(str[6:8]),0,0,0).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    urlretrieve(
+        URL_POSITION.format(
+            collection='nclimgrid-monthly',
+            lon=lon, lat=lat,
+            param=param.value,
+            start=str2dt(start),
+            end=str2dt(end)),
+        out_filepath)
+    
+    return out_filepath
