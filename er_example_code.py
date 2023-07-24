a6d341a2fb7696cefe26f1b5bf8d2b36d56813b2
sam-laptop-vm <seadas-user@localhost>
2023-07-24 11:24:31 +0100

    Documented different spatial calling options #26

--- er_example_code.py
+++ er_example_code.py
@@ -7,10 +7,23 @@ wps = WebProcessingService("http://api.pixalytics.com/climate/wps", skip_caps=Tr
 wps.getcapabilities()
 
 # Execute
+calltype= 'point'
+if calltype == 'box':
+    latvals = '[49.03 , 60.00]'
+    lonvals = '[-101.35, -89.16]'
+elif calltype == 'polygon': # Manitoba example
+    latvals = '[59.98 , 58.66, 58.63, 57.17, 57.12, 56.63, 52.79, 49.03, 49.06, 60.00, 59.98]'
+    lonvals = '[-94.80, -94.75, -93.01, -92.54, -90.58, -89.16, -95.17, -95.17, -101.35, -102.03, -94.80]'
+else: #point
+    latvals = '55.5'
+    lonvals = '-99.1'
+
+print("Making a call as a {}".format(calltype))
+
 inputs = [ ("start_date", '20200101'),
         ("end_date", '20221231'),
-        ("latitude", '55.5'),
-        ("longitude", '-99.1')]
+        ("latitude", latvals),
+        ("longitude", lonvals)]
 
 execution = wps.execute("drought", inputs, "output")
 
